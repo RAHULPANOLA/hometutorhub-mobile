ce55e347ec4a1ec81b140bd113bb1cdbf6f91883
RAHULPANOLA <rcoolest92@gmail.com>
2026-06-26 16:42:24 +0530

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,9 +1,5 @@
 class AppConfig {
-  // Production API (deployed on Render)
-  static const String baseUrl = 'https://hometutorhub-1.onrender.com/';
+  // Production URL (Host it Smart)
+  static const String baseUrl = 'http://hometutorhub.in/api.hometutorhub.in';
   static const String apiUrl = '$baseUrl/api';
-  
-  // For local testing, uncomment this:
-  // static const String baseUrl = 'http://10.106.9.55:5000';
-  // static const String apiUrl = '$baseUrl/api';
 }
